c4aa64a785d66bce9e609f684bba496108c9d308
brunood07 <brunood07@gmail.com>
2023-10-12 11:37:10 -0300

    terceira e quarta aula

---
+++ src/main/java/br/com/brunood/todolist/task/TaskController.java
@@ -0,0 +1,57 @@
+package br.com.brunood.todolist.task;
+
+import br.com.brunood.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    ITaskRepository taskRepository;
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody TaskModel data, HttpServletRequest request) {
+        data.setIdUser((UUID) request.getAttribute("idUser"));
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(data.getStartAt()) || currentDate.isAfter(data.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de ínicio/término deve ser maior que a data atual");
+        }
+
+        if (data.getStartAt().isAfter(data.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de ínicio deve ser menor que a data de término");
+        }
+        var task = this.taskRepository.save(data);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskModel>> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return ResponseEntity.status(HttpStatus.OK).body(tasks);
+    }
+
+    @PutMapping("/{taskId}")
+    public ResponseEntity update(@RequestBody TaskModel data, @PathParam("/{taskId}") UUID taskId,  HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var task = this.taskRepository.findById(taskId).orElse(null);
+        Utils.copyNonNullProperties(data, task);
+
+        this.taskRepository.save(task);
+
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+}
--- src/main/java/br/com/brunood/todolist/user/UserController.java
+++ src/main/java/br/com/brunood/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.brunood.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,9 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, data.getPassword().toCharArray());
+
+        data.setPassword(passwordHashed);
         var userCreated = this.userRepository.save(data);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
